b1727dca73c4c3e3ccc04c82d081931cfa063f49
n9maple <goodsssdd1@gmail.com>
2025-04-06 16:10:51 +0000

    code improvement and formatting

--- number_prediction.py
+++ number_prediction.py
@@ -1,53 +1,84 @@
 import json
 import pandas as pd
 import argparse
-from rich.progress import Progress, TimeElapsedColumn, TimeRemainingColumn, BarColumn, TextColumn
+from rich.progress import (
+    Progress,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+    BarColumn,
+    TextColumn,
+)
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--box_pred_path", type=str, default="save_result/pred.json", help="Path to the box prediction json file")
-    parser.add_argument("--save_path", type=str, default="save_result/pred.csv", help="Path to the saved result")
-    parser.add_argument("--score_thre", type=float, default=0.8, help="The score threshold to pick the box")
+    parser.add_argument(
+        "--box_pred_path",
+        type=str,
+        default="save_result/pred.json",
+        help="Path to the box prediction json file",
+    )
+    parser.add_argument(
+        "--save_path",
+        type=str,
+        default="save_result/pred.csv",
+        help="Path to the saved result",
+    )
+    parser.add_argument(
+        "--score_thre",
+        type=float,
+        default=0.8,
+        help="The score threshold to pick the box",
+    )
     args = parser.parse_args()
     num_pred(args.box_pred_path, args.save_path, args.score_thre)
+
 
 def num_pred(box_pred_path, save_path, score_thre):
     with open(box_pred_path, "r") as f:
         box_data = json.load(f)
 
-    columns = ['image_id', 'pred_label']
+    columns = ["image_id", "pred_label"]
     pred_df = pd.DataFrame(columns=columns)
 
-    box_dict = {} # {image_id:[(digit, x_center_coordinate)]}
+    box_dict = {}  # {image_id:[(digit, x_center_coordinate)]}
 
     with Progress(
         TextColumn("[progress.description]{task.description}"),
         BarColumn(),
         "[progress.percentage]{task.percentage:>3.1f}%",
         TimeElapsedColumn(),
-        TimeRemainingColumn()
+        TimeRemainingColumn(),
     ) as progress:
         task = progress.add_task("[green]Filter boxes...", total=len(box_data))
         for box in box_data:
-            if box['score']>=score_thre:
-                x_center = box['bbox'][0]+box['bbox'][2]/2 # find the x-center of the box
-                box_dict.setdefault(box['image_id'], []).append((box['category_id']-1, x_center))
+            if box["score"] >= score_thre:
+                x_center = (
+                    box["bbox"][0] + box["bbox"][2] / 2
+                )  # find the x-center of the box
+                box_dict.setdefault(box["image_id"], []).append(
+                    (box["category_id"] - 1, x_center)
+                )
             progress.update(task, advance=1)
-    
+
     with Progress(
         TextColumn("[progress.description]{task.description}"),
         BarColumn(),
         "[progress.percentage]{task.percentage:>3.1f}%",
         TimeElapsedColumn(),
-        TimeRemainingColumn()
+        TimeRemainingColumn(),
     ) as progress:
         task = progress.add_task("[cyan]Make predictions...", total=len(box_dict))
         for image_id in box_dict:
-            sorted_digit = sorted(box_dict[image_id], key=lambda x: x[1]) # sort by x_center
-            pred = ''.join(str(t[0]) for t in sorted_digit)
+            sorted_digit = sorted(
+                box_dict[image_id], key=lambda x: x[1]
+            )  # sort by x_center
+            pred = "".join(str(t[0]) for t in sorted_digit)
             pred_df.loc[len(pred_df)] = [image_id, pred]
             progress.update(task, advance=1)
-    
+
     pred_df.to_csv(save_path, index=False)
+
+
 if __name__ == "__main__":
     main()
--- train.py
+++ train.py
@@ -2,15 +2,18 @@ import torch
 import torchvision
 import argparse
 from torch.utils.data import DataLoader
-from torchvision.models.detection.faster_rcnn import FasterRCNN_ResNet50_FPN_V2_Weights, FastRCNNPredictor
+from torchvision.models.detection.faster_rcnn import (
+    FasterRCNN_ResNet50_FPN_V2_Weights,
+    FastRCNNPredictor,
+)
 from tqdm import tqdm
 from Dataset import TrainDataset
 from torch.utils.data import Subset
 from map import COCOmap
 import os
 
+
 def train_one_epoch(model, train_loader, optimizer, device):
-    """ 訓練一個 epoch，並回傳訓練損失 """
     model.train()
     total_train_loss = 0.0
     train_pbar = tqdm(train_loader, desc="Training", leave=False)
@@ -20,7 +23,7 @@ def train_one_epoch(model, train_loader, optimizer, device):
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
         optimizer.zero_grad()
-        loss_dict = model(images, targets)  # 計算 loss
+        loss_dict = model(images, targets)
         loss = sum(loss for loss in loss_dict.values())
         loss.backward()
         optimizer.step()
@@ -33,7 +36,6 @@ def train_one_epoch(model, train_loader, optimizer, device):
 
 
 def validate(model, valid_loader, device):
-    """ 驗證模型，計算 Validation mAP """
     model.eval()
     all_preds, all_gts = [], []
 
@@ -42,74 +44,152 @@ def validate(model, valid_loader, device):
         for images, targets in valid_pbar:
             images = [img.to(device) for img in images]
             targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-
-            # 只使用 images 進行前向傳播，因為驗證階段不需要 loss
             preds = model(images)
-            
-            # 獲取預測結果
             all_preds.extend(single_img_pred for single_img_pred in preds)
             all_gts.extend(single_img_gt for single_img_gt in targets)
-                
 
-    # 計算 mAP
     mAP = COCOmap(all_preds, all_gts)
-
     return mAP
-
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--train_root", type=str, default="data/train", help="Path to training images")
-    parser.add_argument("--train_annotation", type=str, default="data/train.json", help="Path to COCO format training annotation file")
-    parser.add_argument("--valid_root", type=str, default="data/valid", help="Path to validation images")
-    parser.add_argument("--valid_annotation", type=str, default="data/valid.json", help="Path to COCO format validation annotation file")
-    parser.add_argument("--batch_size", type=int, default=4, help="Batch size for training")
+    parser.add_argument(
+        "--train_root", type=str, default="data/train", help="Path to training images"
+    )
+    parser.add_argument(
+        "--train_annotation",
+        type=str,
+        default="data/train.json",
+        help="Path to COCO format training annotation file",
+    )
+    parser.add_argument(
+        "--valid_root", type=str, default="data/valid", help="Path to validation images"
+    )
+    parser.add_argument(
+        "--valid_annotation",
+        type=str,
+        default="data/valid.json",
+        help="Path to COCO format validation annotation file",
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=4, help="Batch size for training"
+    )
     parser.add_argument("--lr", type=float, default=0.005, help="Learning rate")
-    parser.add_argument("--epochs", type=int, default=10, help="Number of training epochs")
-    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Device to use for training")
-    parser.add_argument("--num_workers", type=int, default=4, help="Number of worker threads for DataLoader")
-    parser.add_argument("--save_dir", type=str, default="save_model", help="The directory where the weight is saved")
+    parser.add_argument(
+        "--epochs", type=int, default=10, help="Number of training epochs"
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cuda" if torch.cuda.is_available() else "cpu",
+        help="Device to use for training",
+    )
+    parser.add_argument(
+        "--num_workers",
+        type=int,
+        default=4,
+        help="Number of worker threads for DataLoader",
+    )
+    parser.add_argument(
+        "--save_dir",
+        type=str,
+        default="save_model",
+        help="The directory where the weight is saved",
+    )
+    parser.add_argument(
+        "--partial_training_data",
+        type=float,
+        default=-1,
+        help="The ratio of training data to be used. If the ratio is not between 0 and 1, all the data will be used to train",
+    )
+    parser.add_argument(
+        "--partial_validation_data",
+        type=float,
+        default=-1,
+        help="The ratio of validation data to be used. If the ratio is not between 0 and 1, all the data will be used to validate",
+    )
     args = parser.parse_args()
 
-
-    # 初始化模型
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(weights=FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT)
+    # initialize model
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(
+        weights=FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+    )
     num_classes = 11
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
     model.to(args.device)
 
-    # 加載數據集
-    train_dataset = TrainDataset(args.train_root, args.train_annotation, transform=torchvision.transforms.ToTensor())
-    valid_dataset = TrainDataset(args.valid_root, args.valid_annotation, transform=torchvision.transforms.ToTensor())
-    #### Test Code
-    # subset_size = 50
-    # indices = list(range(subset_size))  # 取前 100 筆資料
-    # train_subset = Subset(train_dataset, indices)
-    # val_subset = Subset(valid_dataset, indices)
+    # load dataset
+    train_dataset = TrainDataset(
+        args.train_root,
+        args.train_annotation,
+        transform=torchvision.transforms.ToTensor(),
+    )
+    valid_dataset = TrainDataset(
+        args.valid_root,
+        args.valid_annotation,
+        transform=torchvision.transforms.ToTensor(),
+    )
 
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, collate_fn=lambda x: tuple(zip(*x)))
-    valid_loader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, collate_fn=lambda x: tuple(zip(*x)))
+    # use partial dataset
+    if args.partial_training_data < 1 and args.partial_training_data > 0:
+        subset_size = len(train_dataset) * args.partial_training_data
+        indices = list(range(subset_size))
+        train_subset = Subset(train_dataset, indices)
+        train_loader = DataLoader(
+            train_subset,
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=args.num_workers,
+            collate_fn=lambda x: tuple(zip(*x)),
+        )
+    else:
+        train_loader = DataLoader(
+            train_dataset,
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=args.num_workers,
+            collate_fn=lambda x: tuple(zip(*x)),
+        )
 
-    # 設定優化器
-    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4)
+    if args.partial_validation_data < 1 and args.partial_validation_data > 0:
+        subset_size = len(valid_dataset) * args.partial_validation_data
+        indices = list(range(subset_size))
+        valid_subset = Subset(train_dataset, indices)
+        valid_loader = DataLoader(
+            valid_subset,
+            batch_size=args.batch_size,
+            shuffle=False,
+            num_workers=args.num_workers,
+            collate_fn=lambda x: tuple(zip(*x)),
+        )
+    else:
+        valid_loader = DataLoader(
+            valid_dataset,
+            batch_size=args.batch_size,
+            shuffle=False,
+            num_workers=args.num_workers,
+            collate_fn=lambda x: tuple(zip(*x)),
+        )
 
-    # 創建儲存模型的資料夾
-    
+    # set optimizer
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4
+    )
     os.makedirs(args.save_dir, exist_ok=True)
-    # 訓練循環
     for epoch in range(args.epochs):
         print(f"\nEpoch [{epoch+1}/{args.epochs}]")
 
         train_loss = train_one_epoch(model, train_loader, optimizer, args.device)
         mAP = validate(model, valid_loader, args.device)
 
-        print(f"Train Loss: {train_loss:.4f} | mAP: {mAP:.4f}")
+        print(f"Train Loss: {train_loss:.4f} | Validation mAP: {mAP:.4f}")
 
-        # 儲存模型
+        # save model
         torch.save(model.state_dict(), f"{args.save_dir}/epoch{epoch+1}.pth")
+        print(f"save model in {args.save_dir}/epoch{epoch+1}.pth")
 
 
 if __name__ == "__main__":
